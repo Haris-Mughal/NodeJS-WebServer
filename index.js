13650e888555f91f7a7815ffb18b00b8595aec92
Haris Mughal <harismirza3456@gmail.com>
2024-11-01 23:28:26 +0500

    query parameters - url

--- index.js
+++ index.js
@@ -1,34 +1,69 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const port = 8000;
 const time = new Date().getTime();
 
 const myServer = http.createServer((req, res) => {
-    console.log("Which Page is this ?");
-    // console.log(req.headers.toString());
+    // console.log("Which Page is this ?");
+    if (req.url === "/favicon.ico") return res.end();
+
+    const myUrl = url.parse(req.url, true);
+    // console.log(myUrl);
 
     fs.appendFile(
         "requestLogs.txt",
-        `${time}: ${req.url} - ${
-            req.url === "/"
-                ? "Home Page"
-                : req.url === "/about"
-                ? "About Page"
-                : req.url === "/favicon.ico"
-                ? "Demo"
-                : "404 Not Found"
-        }\n`,
+        `${time}: ${req.method} ${req.url} - Request added\n`,
         (err) => {
-            switch (req.url) {
-                case "/":
-                    res.end("My Home Page");
-                    break;
-                case "/about":
-                    res.end("My About Page");
-                    break;
-                default:
-                    res.end("404 Not Found");
+            if (!err) {
+                switch (myUrl.pathname) {
+                    case "/":
+                        res.end("My Home Page");
+                        break;
+                    case "/about":
+                        const name = myUrl.query.name;
+                        const id = myUrl.query.id;
+                        const age = myUrl.query.age;
+                        if (name && id && age) {
+                            res.end(
+                                `User's name is ${name} and his ID is ${id} and his age is ${age}.`
+                            );
+                        } else if (name && id) {
+                            res.end(
+                                `User's name is ${name} and his ID is ${id}.`
+                            );
+                        } else if (name) {
+                            res.end(`User's name is ${name}.`);
+                        } else if (id) {
+                            res.end(`User's ID is ${id}.`);
+                        } else if (age) {
+                            res.end(`User's age is ${age}.`);
+                        } else {
+                            res.end("This is About Page.");
+                        }
+                        break;
+                    case "/search":
+                        const searchQuery = myUrl.query.q;
+                        if (searchQuery) {
+                            res.end(`Here are the results for ${searchQuery}:`);
+                        } else {
+                            res.end("No search query provided.");
+                        }
+                        break;
+                    case "/signup":
+                        if (req.method === "GET") {
+                            res.end("This is Sign Up Page");
+                        } else {
+                            if (req.method === "POST") {
+                                // DB Query - sending data to DB
+                                res.end("Success");
+                            }
+                        }
+                        break;
+                    default:
+                        res.end("404 Not Found");
+                }
             }
         }
     );
